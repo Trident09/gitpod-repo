62057d05206615e3972b55918ee1dff2f7b71d94
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2024-04-13 11:28:58 +0000

    main program

---
+++ api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Trident09/url-shortner/routes"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func setUpRoutes(app *fiber.App) {
+	app.Get("/:url", routes.ResolveURL)
+	app.Post("/api/shorten", routes.ShortenURL)
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	app := fiber.New()
+
+	app.Use(logger.New())
+
+	setUpRoutes(app)
+
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+}
